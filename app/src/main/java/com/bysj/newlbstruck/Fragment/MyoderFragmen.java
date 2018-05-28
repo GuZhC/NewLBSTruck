d0d2a1352aa6c2fdff8438351c44fabc5e8fe2d9
ZhaiNiao <835166582@qq.com>
2018-05-28 19:52:49 +0800

    添加订单页面

---
+++ app/src/main/java/com/bysj/newlbstruck/Fragment/MyoderFragmen.java
@@ -0,0 +1,23 @@
+package com.bysj.newlbstruck.Fragment;
+
+import android.os.Bundle;
+
+import com.bysj.newlbstruck.Activity.BaseFragment;
+import com.bysj.newlbstruck.R;
+
+/**
+ * Created by guZhongC on 2018/5/28.
+ * describe:
+ */
+
+public class MyoderFragmen extends BaseFragment {
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.fragment_my_oder;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+}
