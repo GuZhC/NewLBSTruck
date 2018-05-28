2bd286835543ad40981e96ecb29a08d24ea3ac90
ZhaiNiao <835166582@qq.com>
2018-05-28 22:34:31 +0800

    司机用户发布信息

--- app/src/main/java/com/bysj/newlbstruck/Activity/PublishDriveActivity.java
+++ app/src/main/java/com/bysj/newlbstruck/Activity/PublishDriveActivity.java
@@ -16,7 +16,7 @@ import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
 import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.bigkoo.pickerview.view.TimePickerView;
-import com.bysj.newlbstruck.Bean.UserOrder;
+import com.bysj.newlbstruck.Bean.DriverOrder;
 import com.bysj.newlbstruck.Constant;
 import com.bysj.newlbstruck.R;
 import com.bysj.newlbstruck.utils.SharedPreferenceUtil;
@@ -37,121 +37,78 @@ import cn.bmob.v3.listener.SaveListener;
 //发布订单
 public class PublishDriveActivity extends BaseActivity {
 
-    @BindView(R.id.et_addgoods_type)
-    EditText etAddgoodsType;
-    @BindView(R.id.et_addgoods_num)
-    EditText etAddgoodsNum;
-    @BindView(R.id.et_addgoods_weight)
-    EditText etAddgoodsWeight;
-    @BindView(R.id.et_addgoods_baozhuangtype)
-    EditText etAddgoodsBaozhuangtype;
-    @BindView(R.id.et_addgoods_goodssize)
-    EditText etAddgoodsGoodssize;
-    @BindView(R.id.et_addgoods_time)
-    TextView etAddgoodsTime;
-    @BindView(R.id.et_addgoods_time_two)
-    TextView etAddgoodsTimeTwo;
-    @BindView(R.id.et_addgoods_money)
-    TextView etAddgoodsMoney;
-    @BindView(R.id.et_addgoods_location)
-    EditText etAddgoodsLocation;
-    @BindView(R.id.et_addgoods_locationtwo)
-    EditText etAddgoodsLocationtwo;
-    @BindView(R.id.btn_addgoods)
-    Button btnAddgoods;
+
+    @BindView(R.id.et_tujindi)
+    EditText etTujindi;
+    @BindView(R.id.et_huowuleibie)
+    EditText etHuowuleibie;
+    @BindView(R.id.et_yunfeixianzhi)
+    TextView etYunfeixianzhi;
+    @BindView(R.id.et_konyudunwei)
+    EditText etKonyudunwei;
+    @BindView(R.id.et_mudidi)
+    EditText etMudidi;
+    @BindView(R.id.et_facheshijian)
+    TextView etFacheshijian;
+    @BindView(R.id.et_chufadi)
+    EditText etChufadi;
+    @BindView(R.id.et_chexianchicun)
+    EditText etChexianchicun;
+    @BindView(R.id.et_qichexinhao)
+    EditText etQichexinhao;
+    @BindView(R.id.et_yunshudunwei)
+    EditText etYunshudunwei;
+    @BindView(R.id.btn_adddrive)
+    Button btnAdddrive;
+    @BindView(R.id.et_daodashijian)
+    TextView etDaodashijian;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_publish);
+        setContentView(R.layout.activity_publishdrive);
         ButterKnife.bind(this);
         setBackBtn();
         setTitle("信息填写");
     }
 
-    @OnClick({R.id.et_addgoods_time, R.id.et_addgoods_time_two, R.id.et_addgoods_money, R.id.btn_addgoods})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.et_addgoods_time:
-                TimePickerView pvTime = new TimePickerBuilder(PublishDriveActivity.this, new OnTimeSelectListener() {
-                    @Override
-                    public void onTimeSelect(Date date, View v) {
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                        etAddgoodsTime.setText(sdf.format(date));
-                    }
-                })
-                        .setTextColorCenter(getResources().getColor(R.color.blue)) //设置选中项文字颜色
-                        .build();
-                pvTime.show();
-                break;
-            case R.id.et_addgoods_time_two:
-                TimePickerView pvTimetwo = new TimePickerBuilder(PublishDriveActivity.this, new OnTimeSelectListener() {
-                    @Override
-                    public void onTimeSelect(Date date, View v) {
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                        etAddgoodsTimeTwo.setText(sdf.format(date));
-                    }
-                })
-                        .setTextColorCenter(getResources().getColor(R.color.blue)) //设置选中项文字颜色
-                        .build();
-                pvTimetwo.show();
-                break;
-            case R.id.et_addgoods_money:
-                final List<String> datasB = new LinkedList<>();
-                datasB.add("100以下");
-                datasB.add("100-300");
-                datasB.add("300-500");
-                datasB.add("500-700");
-                datasB.add("700-1000");
-                datasB.add("1000-1500");
-                datasB.add("1500-2000");
-                datasB.add("2000以上");
-                hideKeyboard(etAddgoodsMoney.getWindowToken());
-                optionsPicker(datasB, etAddgoodsMoney);
-                break;
-            case R.id.btn_addgoods:
-                doPost();
-                break;
-        }
-    }
-
     private void doPost() {
-        if (TextUtils.isEmpty(etAddgoodsNum.getText().toString()) || TextUtils.isEmpty(etAddgoodsWeight.getText().toString()) ||
-                TextUtils.isEmpty(etAddgoodsBaozhuangtype.getText().toString()) || TextUtils.isEmpty(etAddgoodsGoodssize.getText().toString()) ||
-                TextUtils.isEmpty(etAddgoodsTime.getText().toString()) || TextUtils.isEmpty(etAddgoodsTimeTwo.getText().toString()) ||
-                TextUtils.isEmpty(etAddgoodsMoney.getText().toString()) || TextUtils.isEmpty(etAddgoodsLocation.getText().toString()) ||
-                TextUtils.isEmpty(etAddgoodsLocationtwo.getText().toString()) || TextUtils.isEmpty(etAddgoodsType.getText().toString())) {
+        if (TextUtils.isEmpty(etTujindi.getText().toString()) || TextUtils.isEmpty(etHuowuleibie.getText().toString()) ||
+                TextUtils.isEmpty(etYunfeixianzhi.getText().toString()) || TextUtils.isEmpty(etKonyudunwei.getText().toString()) ||
+                TextUtils.isEmpty(etMudidi.getText().toString()) || TextUtils.isEmpty(etFacheshijian.getText().toString()) ||
+                TextUtils.isEmpty(etChufadi.getText().toString()) || TextUtils.isEmpty(etChexianchicun.getText().toString()) ||
+                TextUtils.isEmpty(etQichexinhao.getText().toString()) || TextUtils.isEmpty(etYunshudunwei.getText().toString())) {
             ToastUtils.showError(PublishDriveActivity.this, "请填写完整信息");
         } else {
+            DriverOrder driverOrder = new DriverOrder();
+            driverOrder.setArrivalTime(etDaodashijian.getText().toString());
+            driverOrder.setDriverId(SharedPreferenceUtil.instance(this).getString(Constant.USER_ID));
+            driverOrder.setStartPointLat("11111");
+            driverOrder.setStartPointLng("22222");
+            driverOrder.setPathways(etTujindi.getText().toString());
+            driverOrder.setHopeGoodsClass(etHuowuleibie.getText().toString());
+            driverOrder.setFreightLimit(etYunfeixianzhi.getText().toString());
+            driverOrder.setFreeTonnage(etKonyudunwei.getText().toString());
+            driverOrder.setDestination(etMudidi.getText().toString());
 
-            UserOrder userOrder = new UserOrder();
-            userOrder.setCategoryGoods(etAddgoodsType.getText().toString());
-            userOrder.setUserId(SharedPreferenceUtil.instance(this).getString(Constant.USER_ID));
-            userOrder.setStartPointLat("11111");
-            userOrder.setStartPointLng("22222");
-            userOrder.setReceiptTime(etAddgoodsTimeTwo.getText().toString());
-            userOrder.setReceiptPlace(etAddgoodsLocationtwo.getText().toString());
-            userOrder.setPackingForm(etAddgoodsBaozhuangtype.getText().toString());
-            userOrder.setPackagingSize(etAddgoodsGoodssize.getText().toString());
-            userOrder.setNumber(etAddgoodsNum.getText().toString());
-
-            userOrder.setFreightLimit(etAddgoodsMoney.getText().toString());
-            userOrder.setEndPointLat("33333");
-            userOrder.setEndPointLng("444444");
-            userOrder.setDeliveryTime(etAddgoodsTime.getText().toString());
-            userOrder.setDeliveryPlace(etAddgoodsLocation.getText().toString());
-            userOrder.setWeight(etAddgoodsWeight.getText().toString());
-            userOrder.save(new SaveListener<String>() {
-                               @Override
-                               public void done(String s, BmobException e) {
-                                   if (e == null) {
-                                       ToastUtils.showSuccess(PublishDriveActivity.this, "发布成功");
-                                       finish();
-                                   } else {
-                                       ToastUtils.showError(PublishDriveActivity.this, "请填写完整信息|" + e.getMessage());
-                                   }
-                               }
-                           }
+            driverOrder.setDepartureTime(etFacheshijian.getText().toString());
+            driverOrder.setEndPointLat("33333");
+            driverOrder.setEndPointLng("444444");
+            driverOrder.setDeparturePlace(etChufadi.getText().toString());
+            driverOrder.setCarSize(etChexianchicun.getText().toString());
+            driverOrder.setCarModel(etQichexinhao.getText().toString());
+            driverOrder.setTransportTonnage(etYunshudunwei.getText().toString());
+            driverOrder.save(new SaveListener<String>() {
+                                 @Override
+                                 public void done(String s, BmobException e) {
+                                     if (e == null) {
+                                         ToastUtils.showSuccess(PublishDriveActivity.this, "发布成功");
+                                         finish();
+                                     } else {
+                                         ToastUtils.showError(PublishDriveActivity.this, "请填写完整信息|" + e.getMessage());
+                                     }
+                                 }
+                             }
             );
         }
     }
@@ -176,4 +133,51 @@ public class PublishDriveActivity extends BaseActivity {
         pvOptions.setPicker(datas);
         pvOptions.show();
     }
+
+    @OnClick({R.id.et_yunfeixianzhi, R.id.btn_adddrive,R.id.et_daodashijian,R.id.et_facheshijian})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.et_yunfeixianzhi:
+                final List<String> datasB = new LinkedList<>();
+                datasB.add("100以下");
+                datasB.add("100-300");
+                datasB.add("300-500");
+                datasB.add("500-700");
+                datasB.add("700-1000");
+                datasB.add("1000-1500");
+                datasB.add("1500-2000");
+                datasB.add("2000以上");
+                hideKeyboard(etYunfeixianzhi.getWindowToken());
+                optionsPicker(datasB, etYunfeixianzhi);
+                break;
+            case R.id.btn_adddrive:
+                doPost();
+                break;
+                case R.id.et_daodashijian:
+                    TimePickerView pvTime = new TimePickerBuilder(PublishDriveActivity.this, new OnTimeSelectListener() {
+                        @Override
+                        public void onTimeSelect(Date date, View v) {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                            etDaodashijian.setText(sdf.format(date));
+                        }
+                    })
+                            .setTextColorCenter(getResources().getColor(R.color.blue)) //设置选中项文字颜色
+                            .build();
+                    pvTime.show();
+                break;
+                case R.id.et_facheshijian:
+                    TimePickerView pvTimetwo = new TimePickerBuilder(PublishDriveActivity.this, new OnTimeSelectListener() {
+                        @Override
+                        public void onTimeSelect(Date date, View v) {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                            etFacheshijian.setText(sdf.format(date));
+                        }
+                    })
+                            .setTextColorCenter(getResources().getColor(R.color.blue)) //设置选中项文字颜色
+                            .build();
+                    pvTimetwo.show();
+                break;
+        }
+    }
+
 }
